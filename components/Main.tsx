97bfc5cfc7740fc215d3737b946cbe92a5d863dd
hironomiu <hironomiu@gmail.com>
2022-07-25 10:40:15 +0900

    commited

--- components/Main.tsx
+++ components/Main.tsx
@@ -44,12 +44,16 @@ const Main = () => {
     <main className="flex flex-col justify-center items-center">
       <div className="py-10">
         <input
-          className="border c"
+          className="border text-4xl rounded p-2 text-center"
           type="text"
           value={place}
+          placeholder="場所を入力"
           onChange={handleChangePlace}
         />
-        <button className="text-2xl text-black" onClick={handleClick}>
+        <button
+          className="text-4xl text-black ml-4 bg-blue-500 h-14 w-28 rounded"
+          onClick={handleClick}
+        >
           検索
         </button>
       </div>
