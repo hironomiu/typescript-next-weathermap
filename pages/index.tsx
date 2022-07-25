53e45fbc4b01ab2b0ee96d4b9134927273e6eb9b
hironomiu <hironomiu@gmail.com>
2022-07-25 12:05:37 +0900

    commited

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[url("/images/sky.jpeg")] h-[100vh] flex flex-col'>
+    <div className='bg-[url("/images/sky.jpeg")] bg-cover h-[100vh] flex flex-col'>
       <Layout />
     </div>
   )
